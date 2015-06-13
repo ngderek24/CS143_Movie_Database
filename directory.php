1b677d552be579f46de6ef88bd6dc028d9c5daa0
tincan24 <tincan24@gmail.com>
2015-06-12 22:07:28 -0700

    Create directory.php

---
+++ directory.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<link rel='stylesheet' type='text/css' href="1C_CSS.css">
+		<style>
+		
+
+		</style>
+	</head>
+
+	<body>
+	<div id='header'>
+		<h2 class='float-left'><a href='directory.php'>CS 143 Project 1C</a></h2>
+		<h3 class='float-right'>Derek Nguyen &amp Sung Hyun Yoon</h3>
+	</div>
+
+	<div id="menu-bar" style="clear:both;">
+		<ul>
+			<li><a href="I1.php">Insert Actor</a></li>
+			<li><a href="I2.php">Insert Movie</a></li>			
+			<li><a href="I3.php">Insert Role</a></li>
+			<li><a href="B1.php">Actor Info</a></li>
+			<li><a href="B2.php">Movie Info</a></li>
+			<li><a href="S1.php">Search</a></li>			
+		</ul>
+	</div>
+
+
+	</body>
+</html>
